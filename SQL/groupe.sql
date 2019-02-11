2f89dbe1768908c8d29309f76d40cec27df2acdf
MatheoDalygit config --global user.name MatheoDalygit config --global user.name MatheoDaly <matego.dofmaster@gmail.com>
2019-02-11 14:17:54 +0100

    ajout de la partie SQL

---
+++ SQL/groupe.sql
@@ -0,0 +1,15 @@
+CREATE TABLE groupe(
+   alim_grp_code   INTEGER  NOT NULL PRIMARY KEY 
+  ,alim_grp_nom_fr VARCHAR(43) NOT NULL
+);
+INSERT INTO groupe(alim_grp_code,alim_grp_nom_fr) VALUES (1,'entrées et plats composés');
+INSERT INTO groupe(alim_grp_code,alim_grp_nom_fr) VALUES (2,'fruits, légumes, légumineuses et oléagineux');
+INSERT INTO groupe(alim_grp_code,alim_grp_nom_fr) VALUES (3,'produits céréaliers');
+INSERT INTO groupe(alim_grp_code,alim_grp_nom_fr) VALUES (4,'viandes, œufs, poissons');
+INSERT INTO groupe(alim_grp_code,alim_grp_nom_fr) VALUES (5,'lait et produits laitiers');
+INSERT INTO groupe(alim_grp_code,alim_grp_nom_fr) VALUES (6,'boissons');
+INSERT INTO groupe(alim_grp_code,alim_grp_nom_fr) VALUES (7,'produits sucrés');
+INSERT INTO groupe(alim_grp_code,alim_grp_nom_fr) VALUES (8,'glaces et sorbets');
+INSERT INTO groupe(alim_grp_code,alim_grp_nom_fr) VALUES (9,'matières grasses');
+INSERT INTO groupe(alim_grp_code,alim_grp_nom_fr) VALUES (10,'aides culinaires et ingrédients divers');
+INSERT INTO groupe(alim_grp_code,alim_grp_nom_fr) VALUES (11,'aliments infantiles');
