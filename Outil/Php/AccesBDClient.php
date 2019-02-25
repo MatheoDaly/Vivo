e85c4958e021b17e727c25d7dad8b33848d9d4bb
RMeuter <roro7@hotmail.fr>
2019-02-25 15:09:36 +0100

    ajout profil

---
+++ Outil/Php/AccesBDClient.php
@@ -0,0 +1,13 @@
+<?php 
+
+function getBD(){
+try
+{
+    return new PDO('mysql:host=localhost;dbname=vivo;charset=utf8','root','');
+}
+catch (Exception $e){
+    die('Erreur :'.$e->getMessage());
+}
+    }
+
+?>
--- Profil/Profil.php
+++ Profil/Profil.php
@@ -3,39 +3,62 @@
 
 <head>
     <meta charset="utf-8">
-    <link href="../Outil/bootstrap-4.3.1-dist/css/bootstrap.min.css">
+    <link href="../Outil/bootstrap-4.3.1-dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="Profil.css" rel="stylesheet">
     <title>Profil</title>
 
 </head>
 
 <body>
+    <?php
+    include('../Outil/Php/AccesBDClient.php');
+    
+    ?>
 
-    <div id="PhotoProfil" class="d-block justify-content-center"><img src="../Image/avatar-1295406_640.png"></div>
+    <div class="d-flex flex-column justify-content-center" id="TableProfil">
 
-    <div class="d-flex" id="TableProfil">
-        <div class="d-flex flex-fill bg-ligth">
-            <div class="d-flex flex-fill bg-white">
-                <div>Profil : didier de la compté des pres jolie
-                    <br> il a 29 ans et il est pas très beau... desolé didier</div>
-                <div>Ajout de logo</div>
-                <div>Objectif perdre 50 kg</div>
+        <div class="independant"><img class="independant" src="../Image/avatar-1295406_640.png" alt="profil"></div>
+
+        <!-- introduction !-->
+
+        <div class="d-flex flex-fill bg-white justify-content-between" id="intro">
+            <div class="col-sm-5">Profil : didier de la compté des pres jolie
+                <br> il a 29 ans et il est pas très beau... desolé didier</div>
+            <div class="col-sm-4">Ajout de logo
             </div>
-            <!--  Intermediare !-->
-            <div>
+            <div class="col-sm-3 d-flex flex-column">
                 <div>
-                    <input type="button" class="btn" value="Consultation des mes statistique">
+                    <strong>Objectif perdre 50 kg
+                    </strong>
                 </div>
+                <div class="d-flex flex-column">
+                    <div class="row">
+                        <img class="col-md-6 icon" name="ordi" src="../Image/mac-1784459_640.png" alt="mac">
+                        <h2 class="col-md-6">
+                            Ordi
+                        </h2>
+                    </div>
+                </div>
+
             </div>
-            <hr>
-            <!-- menue  personnaliser des profil !-->
+        </div>
 
-            <div id="menue"><img src="../Image/food-304597_640.png"></div>
+        <!--  Intermediare !-->
+        <div>
+            <div>
+                <input type="button" class="btn btn-primary" value="Consultation des mes statistique">
+            </div>
+        </div>
+        <hr>
+        <!-- menue  personnaliser des profil !-->
 
-            <div id="ProgMenue">
-                <div>Menue perso
-                    <img src="../Image/menu-3167859_1920.jpg"></div>
-                <div></div>
+        <div class="independant"><img class="independant" src="../Image/food-304597_640.png"></div>
+
+        <!-- Programation des menue !-->
+        <div id="ProgMenue" class="d-flex justify-content-between SousImg">
+            <div>Menue perso
+            </div>
+            <div> Yollo
             </div>
         </div>
 
