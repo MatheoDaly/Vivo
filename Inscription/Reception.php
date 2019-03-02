ba56944824754233b23422cc9a464565269fe80b
RMeuter <roro7@hotmail.fr>
2019-03-02 18:03:35 +0100

    modification de nommage AccesBd.php

--- Inscription/Reception.php
+++ Inscription/Reception.php
@@ -2,7 +2,7 @@
 // Ici ce fichier des infos du java script formulaire.js
 
 //   Detaille variable post : prenom/utilisateur/email/genre/poids/taille/mdp/mdp2
-
+include('../Outil/');
 
 if(isset($_POST['prenom'])&& isset($_POST['taille'])&& isset($_POST['poids'])&& isset($_POST['genre'])&& isset($_POST['email'])&&
   isset($_POST['utilisateur'])
--- Outil/Php/AccesBD.php
+++ Outil/Php/AccesBD.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 function getBD(){
 try
