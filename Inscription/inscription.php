2d7eb88222ea662a24588e3ae4d66a5ba25ab310
RMeuter <roro7@hotmail.fr>
2019-03-02 13:35:53 +0100

    modifiaction inscription et ajout d'un style commun à tous le monde

--- Inscription/inscription.php
+++ Inscription/inscription.php
@@ -4,48 +4,50 @@
 <head>
     <title>Vivo</title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link href="../Outil/Style/Style.css" rel="stylesheet">
+    <link href="../Outil/bootstrap-4.3.1-dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 
 
 <body>
-    <h1>Inscription</h1>
+    <div id="TableProfil" class="d-flex flex-column justify-content-center rounded">
+        <h1>Inscription</h1>
 
-    <form method="get" action="profil.php" autocomplete="off">
-        <div>
+        <form method="get" action="profil.php">
             <div>
                 <p>
-                    Utilisateur :<input type="text" name="utilisateur" value="" />
+                    Utilisateur :<br><input type="text" name="utilisateur" placeholder="Jean Denis" />
                 </p>
                 <p>
-                    Prenom :<input type="text" name="prenom" value="" />
+                    Prenom :<br><input type="text" name="prenom" placeholder="monMail@mail.com" />
                 </p>
                 <p>
-                    E-mail :<input type="text" name="email" value="" />
+                    E-mail :<br><input type="email" name="email" />
                 </p>
-                <p>
+
+                <div class="d-flex justify-content-center align-content-center">
                     Homme :<INPUT type="radio" name="genre" value="M" />
-                </p>
-                <p>
                     Femme :<INPUT type="radio" name="genre" value="F" />
+                </div>
+
+                <p>
+                    Poid (kg) :<br><input type="number" name="poids" placeholder="70 kg" />
                 </p>
                 <p>
-                    Poid (kg) :<input type="number" name="poids" value="" />
+                    Taille (cm) : <br><input type="number" name="taille" placeholder="170 cm" />
                 </p>
                 <p>
-                    Taille (cm) :<input type="number" name="taille" value="" />
+                    Mot de passe :<br><input type="password" name="mdp" />
                 </p>
                 <p>
-                    Mot de passe :<input type="password" name="mdp" value="" />
+                    Confirmation du passe :<br><input type="password" name="mdp2" value="" />
                 </p>
                 <p>
-                    Confirmation du passe :<input type="password" name="mdp2" value="" />
+                    <input type="submit" value="Envoyer" class="btn btn-primary" />
                 </p>
             </div>
-            <p>
-                <input type="submit" value="Envoyer" />
-            </p>
-        </div>
-    </form>
+        </form>
+    </div>
 </body>
 
 </html>
