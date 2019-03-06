b5b843a48ccdc755a9e4baeba3f501c0bd96a82c
RMeuter <roro7@hotmail.fr>
2019-03-06 17:01:46 +0100

    Graphique a personnaliser, mais finit

--- Statistique/Graphique.js
+++ Statistique/Graphique.js
@@ -6,18 +6,18 @@ var myLineChart = new Chart(ctxL, {
     data: {
         labels: ["January", "February", "March", "April", "May", "June", "July"],
         datasets: [{
-                label: "My First dataset",
-                data: [65, 59, 80, 81, 56, 55, 40],
+                label: "Concentration glucose",
+                data: [65, 59, 80, 81, 56, 55, 40], // utiliser une fonction get de ajax pour permettre la capture des données
                 backgroundColor: [
-            'rgba(105, 0, 132, .2)',
+            'rgba(100, 100, 100, .2)',
           ],
                 borderColor: [
-            'rgba(200, 99, 132, .7)',
+            'rgba(200, 200, 200, .7)',
           ],
                 borderWidth: 2
         },
             {
-                label: "My Second dataset",
+                label: "Concentration en sel",
                 data: [28, 48, 40, 19, 86, 27, 90],
                 backgroundColor: [
             'rgba(0, 137, 132, .2)',
--- Statistique/Statistique.php
+++ Statistique/Statistique.php
@@ -18,6 +18,8 @@ $cheminIcon='../Image/IconProgess/';
     <meta charset="utf-8">
     <link href="../Outil/bootstrap-4.3.1-dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="../Profil/Profil.css" rel="stylesheet">
+    <link href="Statistique.css" rel="stylesheet">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <title>Statistique <?php if(isset($Profil)){
     echo ' de '.$Profil[0];
     }
@@ -27,15 +29,17 @@ $cheminIcon='../Image/IconProgess/';
 
 <body>
 
-
     <div class="d-flex flex-column justify-content-center" id="TableProfil">
-        hy
-
+        <h1>Mon graphique jean philipe !</h1>
+        <div id="graphique">
+            <canvas id="lineChart"></canvas>
+        </div>
     </div>
 
 
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.min.js"></script>
+    <script src="Graphique.js" type="text/javascript"></script>
 
 </body>
 
