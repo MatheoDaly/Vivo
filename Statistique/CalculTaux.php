b9d4d4b16bdbc9ff7b053aca28a2eb4bb64281df
RMeuter <roro7@hotmail.fr>
2019-03-13 15:14:34 +0100

    modification pour comprehension

--- Statistique/CalculTaux.php
+++ Statistique/CalculTaux.php
@@ -14,10 +14,17 @@ $req->query("SELECT AVG(aliments.Energie_Règlement_UE_N°_11692011_kcal100g*his
         WHERE historique_aliment.ID_Profil = ".$_SESSION['profil'][0]."
         GROUP BY historique_aliment.Date");
 */
+
+
+
+
+/*
+// Cela me sert seulement pour teste ma fonction en js
 if(isset($_POST['type']) & $_POST['type'] == 1){
     echo json_encode(array(1, 2, 3, 4, 5, 6, 7));
     }
 else if(isset($_POST['type']) & $_POST['type'] == 2) {
     echo json_encode(array(3, 6, 8, 4, 5, 6, 7));
 }
+*/
 ?>
