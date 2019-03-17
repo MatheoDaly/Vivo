24ed75c07edc4f410c15dcf14141136d9753a1e2
RMeuter <roro7@hotmail.fr>
2019-03-17 17:27:21 +0100

    automatisation des menues profil

--- Profil/Profil.php
+++ Profil/Profil.php
@@ -79,43 +79,57 @@ session_start();
                     
                     Adapter le fais que des menue pour aujourd'hui ne sont pas obliger
                     */
-                    
+                    if(isset($_SESSION["profil"])){
+                    $entre=false;
+                    $req = $BD->query("SELECT Date, COUNT(DISTINCT Repas) AS 'NbRepas' from historique_aliment where Date>=NOW() AND ID_Profil=".$_SESSION["profil"]["ID"]." GROUP BY Date");
                     $i=0; // moduler le $i pour adapter le moment des menues
-                    $fin=10;//-> recupere l'intervale de jour en maintenant et le menue le plus tardif !'
-                    while($ligne= $req->fetch()){
-                        if($i==0){ ?>
+                    while($ligne = $req->fetch()){
+                        if($i==0){ $entre=true; ?>
                     <div class="row">
                         <?php } else if ($i%2==0){ ?>
-                        <div class="row">
-                        </div>
+                    </div>
+                    <div class="row">
                         <?php } ?>
-
                         <div class="col-12 col-lg-6">
                             <h3 class="text-center" style="text-decoration:underline;">
                                 <?php switch($i){
                             case 0: echo "Aujourd'hui"; break;
                             case 1: echo "Demain"; break;
-                            default: echo "Dans ".$i." jours"; break;
-                        } ?>
+                            default: echo "Dans ".$i." jours"; break;} ?>
                             </h3>
                             <div class="row">
+                                <?php for($j=0; $j<$ligne["NbRepas"]; $j++){ ?>
                                 <div class="col-12 col-lg-6">
-                                    <h4 class="text-center" style="text-decoration:underline;">Midi</h4>
+                                    <h4 class="text-center" style="text-decoration:underline;">
+                                        <?php switch($j){
+                                            case 0: echo "Midi"; break;
+                                            case 1: echo "Soir"; break;
+                                            default: echo $i." e Repas"; break;} 
+                                        ?>
+                                    </h4>
+                                    <?php
+                                    
+                                    $req1=$BD->query("SELECT aliments.alim_nom_fr AS 'Nom', quantite 
+                                        FROM historique_aliment
+                                        INNER JOIN aliments ON aliments.alim_code = ID_ingredient
+                                        WHERE Repas=".($j+1)."
+                                        AND Date='".$ligne["Date"]."' AND
+                                        AND ID_Profil=".$_SESSION["profil"]["ID"]); // requete archi lourd -> integre le nomà la table historique_aliment ?
+                                    while($ligne1 = $req1->fetch()){
+                                        echo $ligne1["Nom"]." :".$ligne1["quantite"];
+                                    }
+                                    ?>
                                 </div>
-                                <div class="col-12 col-lg-6">
-                                    <h4 class="text-center" style="text-decoration:underline;">Soir</h4>
-                                </div>
+                                <?php } ?>
                             </div>
                         </div>
-
-                        <?php  if($i==$fin){
-                            ?>
-                    </div>
-                    <?php
-                        }
+                        <?php
                         i++;
-                    }
-                    
+                        }
+                    if ($entre){
+                        ?> </div>
+                    <?php           
+                    }}
                     ?>
 
                 </div>
@@ -131,11 +145,12 @@ session_start();
     </div>
 
 
-
-
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <?php
+    if(isset($_SESSION["profil"])){
+        include("ProfilMenue.js");
+    }
+    ?>
 </body>
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
 
 </html>
