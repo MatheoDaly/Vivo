d47bf9690b98a2570be3a44947a8c269bddaf6f7
Romero <juancamilo1955@hotmail.com>
2019-03-25 08:49:59 -0500

    accesBD

--- Outil/Php/AccesBD.php
+++ Outil/Php/AccesBD.php
@@ -1,25 +1,25 @@
 <?php
-
-function getBDMAMPP(){
-try
-{
-    return new PDO('mysql:host=localhost;dbname=vivo;charset=utf8','root','root');
-}
-
-catch (Exception $e){
-    die('Erreur :'.$e->getMessage());
-}
+function getBD(){
+$i=2;
+if($i=1){
+    try{
+        return new PDO('mysql:host=localhost;dbname=vivo;charset=utf8','root','root');
     }
 
-function getBDWAMPP(){
-try
-{
-    return new PDO('mysql:host=localhost;dbname=vivo;charset=utf8','root','');
+    catch (Exception $e){
+        die('Erreur :'.$e->getMessage());
+    }
+}
+    else{
+        try{
+            return new PDO('mysql:host=localhost;dbname=vivo;charset=utf8','root','root');
+        }
+
+        catch (Exception $e){
+            die('Erreur :'.$e->getMessage());
+        }
+    }
 }
 
-catch (Exception $e){
-    die('Erreur :'.$e->getMessage());
-}
-    }
 
 ?>
