a9ff22461de57e0c59b700b21034184a9fe2774f
MatheoDalygit config --global user.name MatheoDalygit config --global user.name MatheoDaly <matego.dofmaster@gmail.com>
2019-03-25 13:54:41 +0100

    modification statistiques

--- Statistique/Statistique.php
+++ Statistique/Statistique.php
@@ -35,6 +35,30 @@ $cheminIcon='../Image/IconProgess/';
         ?></title>
 
 </head>
+<header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-primary">
+            <a class="navbar-brand" href="../index.html">Vivo</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../index.html">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../Profil/Profil.php">Profil</a>
+                    </li>
+                    <li class="nav-item active">
+                        <a class="nav-link" href="../Statistique/Statistique.php">Statistique</a>
+                    </li>
+                </ul>
+                <span class="navbar-text">
+        Pour une bonne santé vivez VIVO !
+      </span>
+            </div>
+        </nav>
+    </header>
 
 <body>
     <!-- Attention mathéo ne modifie pas l'input !-->
