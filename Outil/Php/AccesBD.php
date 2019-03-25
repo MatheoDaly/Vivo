328631a4bc56d2f6dbd5500172b2f9196e5cef51
Romero <juancamilo1955@hotmail.com>
2019-03-25 08:22:51 -0500

    acce bd

--- Outil/Php/AccesBD.php
+++ Outil/Php/AccesBD.php
@@ -3,7 +3,7 @@
 function getBDMAMPP(){
 try
 {
-    return new PDO('mysql:host=localhost;dbname=vivo;charset=utf8','root','');
+    return new PDO('mysql:host=localhost;dbname=vivo;charset=utf8','root','root');
 }
 
 catch (Exception $e){
