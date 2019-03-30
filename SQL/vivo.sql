c0d7cc07af1570fb2ffb614ce254393619091e4d
RMeuter <roro7@hotmail.fr>
2019-03-30 14:07:33 +0100

    modif bd

--- SQL/vivo.sql
+++ SQL/vivo.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.7.9
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mer. 27 mars 2019 à 23:06
--- Version du serveur :  5.7.24
--- Version de PHP :  7.2.14
+-- Généré le :  sam. 30 mars 2019 à 13:06
+-- Version du serveur :  5.7.21
+-- Version de PHP :  5.6.35
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
