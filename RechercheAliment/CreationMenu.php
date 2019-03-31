3f4f5cc406332828814a6e86b152397f7601786e
MatheoDalygit config --global user.name MatheoDalygit config --global user.name MatheoDaly <matego.dofmaster@gmail.com>
2019-03-31 17:51:01 +0200

    modification lien css

--- RechercheAliment/CreationMenu.php
+++ RechercheAliment/CreationMenu.php
@@ -8,7 +8,7 @@ session_start();
 
 <head>
   <meta charset="utf-8">
-  <link rel="stylesheet" href="StyleBis.css" type="text/css">
+  <link rel="stylesheet" href="Style_RechAl.css" type="text/css">
   <title>Creation de menu</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
 </head>
