da49430d0a62baac2ee44f7926543d244b1e8152
Romero <juancamilo1955@hotmail.com>
2019-04-06 06:40:08 -0500

    gout/recherche

--- Gout/gout.php
+++ Gout/gout.php
@@ -1,9 +1,11 @@
 <?php
+//manque change id=1 avec id=$_SESSION['profil']['id']
 include("../Outil/php/AccesBD.php");
 //include_once "Fonctions_alim.php";
 session_start();
 $BD=getBD();
 if(isset($_GET['submit'])){
+    //Ajouter
     if($_GET['submit']=="Ajouter"){
         $i=0;
         $j=0;
@@ -20,7 +22,9 @@ if(isset($_GET['submit'])){
             $i+=1;
         }
     }
-    else{
+    //Eliminer
+    elseif($_GET['submit']=="Eliminer"){
+        //Regime
         if($_GET['type']==1){
             $i=0;
             $j=0;
@@ -37,13 +41,39 @@ if(isset($_GET['submit'])){
                 $i+=1;
             }
         }
-        else{
-            $i=0;
-            while(isset($_GET[$i])){
-            $req=$BD->query("INSERT INTO preferance (`id_Profil`, `id_Aliment`) VALUES ('1', '".$_GET[$i]."')");
-            $i+=1;
+        //aliment
+        else{$i=0;
+            $j=0;
+            while($i<count($_GET)-2) {
+                $fin=false;
+                while(!$fin){
+                    if(isset($_GET[$j])){
+                        $pref=$_GET['type']-2;
+                        $req=$BD->query("DELETE FROM regime_profil WHERE id_Profil=1 and id_Aliment=".$_GET[$j]." and pref=".$pref.")");
+                        $fin=true;
+                    }
+                    $j+=1;
+                }
+                $req->closeCursor();
+                $i+=1;
+            }
+        }
+    }
+    //calendrier
+    else{
+        $i=0;
+        $j=0;
+        while($i<count($_GET)-4) {
+            $fin=false;
+            while(!$fin){
+                if(isset($_GET[$j])){
+                    $req=$BD->query("INSERT INTO regime_profil (`id_Profil`, `id_Regime`,'start','end') VALUES ('1', '".$_GET[$j]."', '".$_GET['start']."', '".$_GET['end']."')");
+                    $fin=true;
+                }
+                $j+=1;
             }
             $req->closeCursor();
+            $i+=1;
         }
     }
 }
@@ -96,7 +126,7 @@ if(isset($_GET['submit'])){
                     <?php $req=$BD->query("SELECT * FROM regime WHERE id in (SELECT id_Regime FROM regime_profil WHERE id_profil=1)");
                     $i=0;
                     while($ligne = $req->fetch()){
-                        echo '<p><input type="checkbox" name="'.$i.'" value="'.$ligne['id'].'"> '.$ligne['Nom'].'</p>';
+                        echo '<p><input type="checkbox" name="'.$i.'" value="'.$ligne['id'].'"> '.$ligne['Nom'].'</p>'."\n";
                         $i+=1;
                     }
                     $req->closeCursor();
@@ -113,7 +143,7 @@ if(isset($_GET['submit'])){
                     <?php $req=$BD->query("SELECT alim_code,alim_nom_fr FROM aliments WHERE alim_code in (SELECT id_Aliment FROM preferance WHERE id_Profil=1 and pref=1)");
                     $i=0;
                     while($ligne = $req->fetch()){
-                        echo '<p><input type="checkbox" name="'.$i.'" value="'.$ligne['id'].'"> '.$ligne['Nom'].'</p>';
+                        echo '<p><input type="checkbox" name="'.$i.'" value="'.$ligne['id'].'"> '.$ligne['Nom'].'</p>'."\n";
                         $i+=1;
                         }
                     $req->closeCursor();
@@ -130,7 +160,7 @@ if(isset($_GET['submit'])){
                     <?php $req=$BD->query("SELECT alim_code,alim_nom_fr FROM aliments WHERE alim_code in (SELECT id_Aliment FROM preferance WHERE id_Profil=1 and pref=0)");
                     $i=0;
                     while($ligne = $req->fetch()){
-                                echo '<p><input type="checkbox" name="'.$i.'" value="'.$ligne['id'].'"> '.$ligne['Nom'].'</p>';
+                                echo '<p><input type="checkbox" name="'.$i.'" value="'.$ligne['id'].'"> '.$ligne['Nom'].'</p>'."\n";
                         $i+=1;
                             }
                     $req->closeCursor();
@@ -148,7 +178,7 @@ if(isset($_GET['submit'])){
                     <?php $req=$BD->query("SELECT * FROM regime WHERE id NOT in (SELECT id_Regime FROM regime_profil WHERE id_profil=1)");
                     $i=0;
                     while($ligne = $req->fetch()){
-                                echo '<p><input type="checkbox" name="'.$i.'" value="'.$ligne['id'].'"> '.$ligne['Nom'].'</p>';
+                                echo '<p><input type="checkbox" name="'.$i.'" value="'.$ligne['id'].'"> '.$ligne['Nom'].'</p>'."\n";
                         $i+=1;
                             }
                     $req->closeCursor();
@@ -159,12 +189,38 @@ if(isset($_GET['submit'])){
                 </div>
                 <div>
                     <h1>Aliment disponible</h1>
-                    <p class="select">
-                        Aliment
-                    </p>
-                    <p class="select">
-                        Aliment
-                    </p>
+                    <div class="partie_recherche">
+                    <form method="get" action="Gout.php" autocomplete="off" id="optionForm">
+                      <br/>
+                      <input type="text" name="Alim">
+                      <input type="submit" name="submit" value="Rechercher">
+                    </form>
+                    <?php
+                      if(isset($_GET['submit']) || isset($_GET['Alim'])){
+                        if(empty($_GET['Alim'])){
+                          echo('<meta http-equiv="refresh" content="0;URL=Choix_Aliment.php">');
+                        }else {
+                            ?>
+                            <form method="get" action="Gout.php">
+                             <?php
+                              $input=$_GET['Alim'];
+                              //$input = preg_replace("#[^0-9a-z]#i","",$input);
+                              $reponse = $BD->query("SELECT * FROM aliments WHERE alim_nom_fr LIKE '%$input%' and alim_grp_code!=1 limit 5");
+                              $i=0;
+                              while($result = $reponse->fetch()){
+                                echo '<p><input type="checkbox" name="'.$i.'" value="'.$result['alim_code'].'"> '.$result['alim_nom_fr'].'</p>'."\n";
+                                 $i+=1;
+                              }
+                              $reponse-> closeCursor();
+                            ?>
+                            <input type="hidden" name="type" value="1">
+                            <input type="submit" class="btn btn-primary" name="submit" value="Ajouter">
+                            </form>
+                            <?php
+                            }
+                          }
+                            ?>
+                    </div>
                 </div>
             </div>
         </div>
