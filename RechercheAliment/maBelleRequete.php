b001f613a9048311f2a0ead625037bd42033d325
MatheoDalygit config --global user.name MatheoDalygit config --global user.name MatheoDaly <matego.dofmaster@gmail.com>
2019-04-07 11:57:26 +0200

    modification choix_menu

--- RechercheAliment/maBelleRequete.php
+++ RechercheAliment/maBelleRequete.php
@@ -2,7 +2,7 @@
     include_once "AccesBD_rechAl.php";
     session_start();
     $bd = getBD();
-    $vegetarien = $bd -> query('SELECT * FROM aliments, groupe, sous_groupe
+    /*$vegan = $bd -> query('SELECT * FROM aliments, groupe, sous_groupe
                                 WHERE groupe.alim_grp_code != 1
                                 AND groupe.alim_grp_code != 4
                                 AND groupe.alim_grp_code != 5
@@ -31,10 +31,47 @@
                                 AND aliments.alim_nom_fr NOT LIKE "Sauce tartare, préemballée"
                                 AND groupe.alim_grp_code = aliments.alim_grp_code
                                 AND sous_groupe.alim_grp_code = aliments.alim_ssgrp_code');
-    while($ligne = $vegetarien ->fetch()){
+    while($ligne = $vegan ->fetch()){
         $add = 'INSERT INTO regime_sans_aliment(id_Aliment, id_Regime) VALUES('.$ligne['alim_code'].', 2)';
         echo $add;
         //$bd -> query($add);
     }
+    $vegan ->closeCursor();*/
+
+/*$sans_porc = $bd -> query('SELECT * FROM aliments, groupe, sous_groupe
+                                WHERE aliments.alim_nom_fr NOT LIKE "% porc"
+                                AND aliments.alim_nom_fr NOT LIKE "porc %"
+                                AND aliments.alim_grp_code != 1
+                                AND sous_groupe.alim_grp_code != 403
+                                AND groupe.alim_grp_code = aliments.alim_grp_code
+                                AND sous_groupe.alim_grp_code = aliments.alim_ssgrp_code');
+    while($ligne = $sans_porc ->fetch()){
+        $add = 'INSERT INTO regime_sans_aliment(id_Aliment, id_Regime) VALUES('.$ligne['alim_code'].', 4)';
+        echo $add;
+        $bd -> query($add);
+    }
+$sans_porc ->  closeCursor();*/
+/*$glucide = $bd -> query('SELECT * FROM `aliments` WHERE aliments.Glucides_g100g > 40');
+    while($ligne = $glucide ->fetch()){
+        $add = 'INSERT INTO est_riche_en(id_aliment, id_nutriment) VALUES('.$ligne['alim_code'].', 1)';
+        echo $add;
+        $bd -> query($add);
+    }
+$glucide ->  closeCursor();*/
         
+/*$lipide = $bd -> query('SELECT * FROM `aliments` WHERE aliments.Lipides_g100g > 15');
+    while($ligne = $lipide ->fetch()){
+        $add = 'INSERT INTO est_riche_en(id_aliment, id_nutriment) VALUES('.$ligne['alim_code'].', 2)';
+        echo $add;
+        $bd -> query($add);
+    }
+$lipide ->  closeCursor();*/
+
+$proteine = $bd -> query('SELECT * FROM `aliments` WHERE aliments.Protéines_g100g > 20');
+    while($ligne = $proteine ->fetch()){
+        $add = 'INSERT INTO est_riche_en(id_aliment, id_nutriment) VALUES('.$ligne['alim_code'].', 3)';
+        echo $add;
+        $bd -> query($add);
+    }
+$proteine ->  closeCursor();
 ?>
