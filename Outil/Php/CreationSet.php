1a6421808dffd5291622636f8832fa89f0c97af2
RMeuter <roro7@hotmail.fr>
2019-04-22 17:07:00 +0200

    maVersion elever par le merge

--- Outil/Php/CreationSet.php
+++ Outil/Php/CreationSet.php
@@ -53,12 +53,12 @@ $j=0;
 $liste=array(5, 2, 1, 3);
 $ListsMoyenneAsso=array(2500, 5, 14, 36, 2);
         
-    for($i=-1;$i<100;$i++){
-            if($i%6 == 0){
+    for($i=-1;$i<1000;$i++){
+            if($i%3 == 0){
                 $j++;
             } 
     for($k=0;$k<$nbVar;$k++){
-        $q = "INSERT INTO statistique VALUES (1, ".(($i%6)+1).", '".$liste[$k]."', ".rand($ListsMoyenneAsso[$k]*0.5 ,$ListsMoyenneAsso[$k]*1.5).", ".$moment."(NOW(), INTERVAL ".$j." DAY), 1)";
+        $q = "INSERT INTO statistique VALUES (1, ".(($i%3)+1).", '".$liste[$k]."', ".rand($ListsMoyenneAsso[$k]*0.5 ,$ListsMoyenneAsso[$k]*1.5).", ".$moment."(NOW(), INTERVAL ".$j." DAY), 1)";
         echo $q."<br>";
         $BD->query($q);
         }
@@ -66,7 +66,7 @@ $ListsMoyenneAsso=array(2500, 5, 14, 36, 2);
     }
     echo "Set repas crée";
 }
-setRepas($BD, 4, "F");
+//setRepas($BD, 4, "P");
 
 function setJour($BD,$nbVar, $moment){
     if($moment=="F"){
@@ -79,9 +79,9 @@ function setJour($BD,$nbVar, $moment){
 $j=0;
 $liste=array(5, 2, 1, 3);
 $ListsMoyenneAsso=array(2500, 5, 14, 36, 2);
-    for($k=0;$k<$nbVar;$k++){
-        
     for($i=0;$i<15;$i++){
+        
+    for($k=0;$k<$nbVar;$k++){
         $q = "INSERT INTO statistique VALUES (2, null, '".$liste[$k]."', ".rand($ListsMoyenneAsso[$k]*0.8 ,$ListsMoyenneAsso[$k]*1.2).", ".$moment."(NOW(), INTERVAL ".$i." DAY), 1)";
         echo $q;
         echo '<br/>';
