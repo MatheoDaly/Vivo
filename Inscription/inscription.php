a9d57b0e1c13e4e2dc0cc37c159b8bb761aa8284
MatheoDalygit config --global user.name MatheoDalygit config --global user.name MatheoDaly <matego.dofmaster@gmail.com>
2019-04-25 17:59:47 +0200

    changement du style de la nav bar

--- Inscription/inscription.php
+++ Inscription/inscription.php
@@ -9,7 +9,7 @@
     <link href="inscription.css" rel="stylesheet">
 </head>
 <header>
-    <nav class="navbar navbar-expand-lg navbar-light">
+    <nav class="navbar navbar-expand-lg navbar-light" style="font-weight: normal; font-size: 16px;">
             <a class="navbar-brand" href="../index.php">Vivo</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
