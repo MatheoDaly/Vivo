bf0c5a396eac31bfce1ad3d5a65c5fc8a6fbf495
RMeuter <roro7@hotmail.fr>
2019-04-27 18:55:44 +0200

    Actualise seulement si Session active

--- Profil/Profil.php
+++ Profil/Profil.php
@@ -7,7 +7,7 @@ include("../Outil/IsTest.php");
 
 include("../Outil/php/AccesBD.php");
 $BD=getBD();
-include("../Actualisation/Actualisation.php");
+if ($testGene || isset($_SESSION['profil']))  include("../Actualisation/Actualisation.php");
 
 if($testStats==true){
         include("../Outil/Php/CreationSet.php");
