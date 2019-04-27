6d1d0d0ee945e5c3d29143869b3c5555cd2ced01
RMeuter <roro7@hotmail.fr>
2019-04-27 23:07:42 +0200

    verification dun bon registrement

--- RechercheAliment/Planification.php
+++ RechercheAliment/Planification.php
@@ -3,17 +3,15 @@
 include_once "AccesBD_rechAl.php";
 $BD =getBD();
 if(isset($_POST["nbtypeMenue"])){
-
+    $add =0;
     //($date, $heure, $id_menu, $id_profil)
     for($i=0; $_POST["nbtypeMenue"]>$i; $i++){
-
         if (isset($_POST['date'.((string)$i)])&&isset($_POST['heure'.((string)$i)])&&isset($_POST['Menu'.((string)$i)])){
-            ajouter($_POST['date'.((string)$i)], $_POST['heure'.((string)$i)], $_POST['Menu'.((string)$i)],1, $BD);
-
-echo ($_POST['date'.((string)$i)]." ".$_POST['heure'.((string)$i)]." ".$_POST['Menu'.((string)$i)]);
+            $add=ajouter($_POST['date'.((string)$i)], $_POST['heure'.((string)$i)], $_POST['Menu'.((string)$i)],1, $BD);
         }
     }
 }
+echo $add;
 //--------------------------------------------Partie fonction
 
 function ajouter($date, $heure, $id_menu, $id_profil,$BD){
@@ -21,7 +19,7 @@ function ajouter($date, $heure, $id_menu, $id_profil,$BD){
 $q = "INSERT INTO menu_profil(date, heure, id_menu, id_profil) VALUES ('".$date."','".$heure."', '".$id_menu."', '".$id_profil."');";
 $BD = getBD();
 $BD->query($q);
-
+return 1;
 }
 
 
