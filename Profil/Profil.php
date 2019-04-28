6f435a73b08834c92badd212ac806c77979e34a3
RMeuter <roro7@hotmail.fr>
2019-04-28 15:10:40 +0200

    modification profil

--- Profil/Profil.php
+++ Profil/Profil.php
@@ -41,45 +41,45 @@ if(isset($_POST['change']) && $_POST['change']=='yes'){
 <body>
     <!-- ################NavBar############### !-->
     <header>
-    <nav class="navbar navbar-expand-lg navbar-light">
-        <a class="navbar-brand" href="../index.php">Vivo</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarText">
-            <ul class="navbar-nav mr-auto">
-               <li class="nav-item">
-                    <a class="nav-link" href="../index.php">Accueil</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../Article/Article.php">Nos articles</a>
-                </li>
-                <?php if(isset($_SESSION['profil']) && !$testGene){ ?>
-                <li class="nav-item active">
-                    <a class="nav-link active" href="../Profil/Profil.php">Profil</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../Statistique/Statistique.php">Statistique</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" id="disconnect" href="../Deconnexion.php">Deconnexion</a>
-                </li>
-                <?php } else { ?>
-                <li class="nav-item">
-                    <a class="nav-link" href="../Inscription/inscription.php">Inscription</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="../Connection/connexion.php">Connexion</a>
-                </li>
-                <?php } ?>
-                
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <a class="navbar-brand" href="../index.php">Vivo</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarText">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href="../index.php">Accueil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../Article/Article.php">Nos articles</a>
+                    </li>
+                    <?php if(isset($_SESSION['profil']) && !$testGene){ ?>
+                    <li class="nav-item active">
+                        <a class="nav-link active" href="../Profil/Profil.php">Profil</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../Statistique/Statistique.php">Statistique</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="disconnect" href="../Deconnexion.php">Deconnexion</a>
+                    </li>
+                    <?php } else { ?>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../Inscription/inscription.php">Inscription</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="../Connection/connexion.php">Connexion</a>
+                    </li>
+                    <?php } ?>
 
-            </ul>
-            <span class="navbar-text">
-                Pour une bonne santé vivez VIVO !
-            </span>
-        </div>
-    </nav>
+
+                </ul>
+                <span class="navbar-text">
+                    Pour une bonne santé vivez VIVO !
+                </span>
+            </div>
+        </nav>
     </header>
 
     <!-- ################ Entete Photo ############### !-->
@@ -128,8 +128,19 @@ if(isset($_POST['change']) && $_POST['change']=='yes'){
                                 <input type="number" name="taille" value="<?php echo $Profil["taille"]; ?>">
                             </div>
                             <div class="p-1 col-6 d-flex flex-column">
-                                Niveau Sportif : (1, 2, 3)
-                                <input type="number" name="lvlSport" value="<?php echo $Profil["poids"]; ?>">
+                                <p>Choisissez votre niveau sportif</p>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="lvlSport" value="1" id="1" />
+                                    <label class="form-check-label" for="1">Peu Sportif</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="lvlSport" value="2" id="2" checked />
+                                    <label class="form-check-label" for="2">Sportif</label>
+                                </div>
+                                <div class="form-check">
+                                    <input class="form-check-input" type="radio" name="lvlSport" value="3" id="3" />
+                                    <label class="form-check-label" for="3">Très Sportif</label>
+                                </div>
                             </div>
                             <div class="p-1 col-6 d-flex flex-column">
                                 Objectif poids :
@@ -238,11 +249,11 @@ if(isset($_POST['change']) && $_POST['change']=='yes'){
     <div id="ProgMenu" class=" rounded row d-flex justify-content-between">
         <div class=" col-10 col-lg-6 bg-dark rounded text-center" style="margin: 50px; padding-top: 25px; padding-bottom: 25px;">
             <h1 class="text-light text-center border border-primary">Menu</h1>
-            <img src="../Image/Icon/icons8-calendrier-64.png"/>
+            <img src="../Image/Icon/icons8-calendrier-64.png" />
             <div style="height: 500px; overflow-y: scroll;">
-            <div class="col-10 bg-light mx-auto rounded ">
-                <!-- Automatiser la gestion du tableau!-->
-                <?php 
+                <div class="col-10 bg-light mx-auto rounded ">
+                    <!-- Automatiser la gestion du tableau!-->
+                    <?php 
                     /* j'execute ma requete, je recupere le nombre de ligne pour ferme le tableau
                     Je ferme et ouvre ma ligne toute les deux cellule -> $i%2 == 0
                     je ferme la div si entre est true !
@@ -256,35 +267,35 @@ if(isset($_POST['change']) && $_POST['change']=='yes'){
                     $i=0; // moduler le $i pour adapter le moment des menus
                     while($ligne = $req->fetch()){ 
                         if($i==0){ $entre=true; ?>
-                <div class="row">
-                    <?php } else if ($i%2==0){ ?>
-                </div>
-                <div class="row">
-                    <?php } ?>
-                    <div class="col-12 col-lg-6">
-                       <div class="p-2 m-2 rounded text-light" style="background-image: linear-gradient(to bottom right, cyan, blue);">
-                        <h3 class="text-center">
-                            <?php echo $ligne['Date']; ?>
+                    <div class="row">
+                        <?php } else if ($i%2==0){ ?>
+                    </div>
+                    <div class="row">
+                        <?php } ?>
+                        <div class="col-12 col-lg-6">
+                            <div class="p-2 m-2 rounded text-light" style="background-image: linear-gradient(to bottom right, cyan, blue);">
+                                <h3 class="text-center">
+                                    <?php echo $ligne['Date']; ?>
 
-                            <?php $req2 = $BD->query("SELECT DISTINCT Repas from historique_aliment where ID_Profil=".$Profil['ID']." AND Date='".$ligne['Date']."' ORDER BY Repas");?>
+                                    <?php $req2 = $BD->query("SELECT DISTINCT Repas from historique_aliment where ID_Profil=".$Profil['ID']." AND Date='".$ligne['Date']."' ORDER BY Repas");?>
 
-                        </h3>
-                        </div>
-                        <div class="row">
-                            <?php while($ligne2= $req2->fetch()){ ?>
-                            <div class="col-12 col-lg-6">
-                               <div class="border border-primary rounded">
-                                <h4 class="text-center">
-                                    <?php switch($ligne2['Repas']){
+                                </h3>
+                            </div>
+                            <div class="row">
+                                <?php while($ligne2= $req2->fetch()){ ?>
+                                <div class="col-12 col-lg-6">
+                                    <div class="border border-primary rounded">
+                                        <h4 class="text-center">
+                                            <?php switch($ligne2['Repas']){
                                             case 9: echo 'Matin'; break;
                                             case 13: echo "Midi"; break;
                                             case 19: echo "Soir"; break;
                                             default: echo "Repas de ".$ligne2['Repas']." heures"; break;} 
                                         ?>
-                                </h4>
-                                </div>
-                                <ul class="liste" style="list-style-image: url('../Image/Icon/icons8-repas-48.png')">
-                                    <?php
+                                        </h4>
+                                    </div>
+                                    <ul class="liste" style="list-style-image: url('../Image/Icon/icons8-repas-48.png')">
+                                        <?php
                                     
                                     $req1=$BD->query("SELECT aliments.alim_nom_fr AS 'Nom', quantite 
                                         FROM historique_aliment
@@ -297,26 +308,26 @@ if(isset($_POST['change']) && $_POST['change']=='yes'){
                                     }
                                     $req1->closeCursor();
                                     ?>
-                                </ul>
+                                    </ul>
+                                </div>
+                                <?php } $req2->closeCursor(); ?>
                             </div>
-                            <?php } $req2->closeCursor(); ?>
                         </div>
+                        <?php $i++;}    $req->closeCursor(); if ($entre){ ?>
                     </div>
-                    <?php $i++;}    $req->closeCursor(); if ($entre){ ?>
-                </div>
-                <?php  } else {
+                    <?php  } else {
                         echo "Vous n'avez aucun menu c'est dommage ! </br> Allez vite vous en faire un, via :<a href='../RechercheAliment/Choix_Menu.php'> Menue</a>";
                     } ?>
-                <a href='../RechercheAliment/Choix_Menu.php'></a>
+                    <a href='../RechercheAliment/Choix_Menu.php'></a>
 
+                </div>
             </div>
-        </div>
 
         </div>
         <?php if($entre){ ?>
         <div class="col-10 col-lg-4 bg-light  rounded" style="margin: 50px; height: 700px; overflow-y: scroll;">
-          <div class="text-center">
-           <img src="../Image/Icon/icons8-liste-64.png"/>
+            <div class="text-center">
+                <img src="../Image/Icon/icons8-liste-64.png" />
             </div>
             <h3 class="text-center">Liste de courses</h3>
             <ul style="list-style-image: url('../Image/Icon/icons8-pomme-48.png')">
