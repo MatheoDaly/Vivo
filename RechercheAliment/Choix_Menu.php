14482b1bc0b0681cba82771b16ace09e76990084
MatheoDalygit config --global user.name MatheoDalygit config --global user.name MatheoDaly <matego.dofmaster@gmail.com>
2019-04-28 11:44:44 +0200

    ajout de favicon

--- RechercheAliment/Choix_Menu.php
+++ RechercheAliment/Choix_Menu.php
@@ -10,7 +10,8 @@ include_once "Fonctions_alim.php";
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="Style_RechAl.css" type="text/css">
-    <title></title>
+    <title>Vivo</title>
+    <link rel="icon" type="image/png" href="../Image/Icon/icons8-aliments-sains-96.png" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script src="../Outil/bootstrap-4.3.1-dist/js/bootstrap.min.js" type="text/javascript"></script>
