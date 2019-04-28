21e708f47db7c471e29a12f73d5cffe18edfec5c
MatheoDalygit config --global user.name MatheoDalygit config --global user.name MatheoDaly <matego.dofmaster@gmail.com>
2019-04-28 11:46:00 +0200

    ajout de favicon

--- RechercheAliment/page_recette.php
+++ RechercheAliment/page_recette.php
@@ -9,7 +9,8 @@ session_start();
 <head>
     <meta charset="utf-8">
     <link rel="stylesheet" href="Style_RechAl.css" type="text/css">
-    <title></title>
+    <title>Vivo</title>
+    <link rel="icon" type="image/png" href="../Image/Icon/icons8-aliments-sains-96.png" />
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
     
 </head>
